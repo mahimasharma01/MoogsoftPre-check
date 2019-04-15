5974ae61938f67000602b50177afe3478e3b57d1
Sharma <mahima-s@dryicelabs.com>
2019-04-15 16:00:38 +0530

    file updated

--- Bin/Zabbix_PreReq.py
+++ Bin/Zabbix_PreReq.py
@@ -59,12 +59,13 @@ Storage=Fun_GetFreeDisk("C:",100)
 if Storage[1]==1:
 	f.write('Check_Disk_Space: Fail ')
 	if Storage[0]=='DNF':
-		f.write('Directory '+args.DriveName+ ' not Found'+"\n")
+		f.write('Directory '+args.DriveName+ ' not found'+"\n")
 	else:
 		f.write("Recommended Value is 100 , Actual Value is "+args(Storage[0])+"\n" )
 elif Storage[1]==0:
 	f.write('Check_Disk_Space: Pass ')
 	f.write("Recommended Value is 100 Actual Value is "+str(Storage[0])+"\n" )
 logger.info('Status from Fun_GetFreeDisk is %s',Storage)
+logger.info('Storage')
 f.close()
 # print Storage
